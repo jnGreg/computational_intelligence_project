8aec544eaa0ffbbff371a389bb08b58c70f6a534
Jan <janpgregor@gmail.com>
2022-03-12 17:03:19 +0100

    added transport task class

--- src/transport_task.py
+++ src/transport_task.py
@@ -1,3 +1,30 @@
 
 class TransportTask:
-    def __init__(self, warehouses:list, trucks:list, points: list):
+    def __init__(self, warehouses: list, trucks: list, points: list):
+        self._warehouses = warehouses
+        self._trucks = trucks
+        self._points = points
+
+    @property
+    def warehouses(self) -> list:
+        return self._warehouses
+
+    @warehouses.setter
+    def warehouses(self, value: list) -> None:
+        self._warehouses = value
+
+    @property
+    def trucks(self) -> list:
+        return self._trucks
+
+    @trucks.setter
+    def trucks(self, value: list) -> None:
+        self._trucks = value
+
+    @property
+    def points(self) -> list:
+        return self._points
+
+    @points.setter
+    def points(self, value: list) -> None:
+        self._points = value
