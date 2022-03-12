b3a1d69da5f20bff349f1a58797caba824511ed3
Jan <janpgregor@gmail.com>
2022-03-12 16:45:51 +0100

    added order class

---
+++ src/order.py
@@ -0,0 +1,25 @@
+
+from point import Point
+from truck import Truck
+
+
+class Order:
+    def __init__(self, point: Point, truck: Truck):
+        self._point = point
+        self._truck = truck
+
+    @property
+    def point(self) -> Point:
+        return self._point
+
+    @point.setter
+    def point(self, value: Point) -> None:
+        self._point = value
+        
+    @property
+    def truck(self) -> Truck:
+        return self._truck
+
+    @truck.setter
+    def truck(self, value: Truck) -> None:
+        self._truck = value
