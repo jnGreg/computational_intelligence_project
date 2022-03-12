28de73b1881454c8916d3496e1087783a7b07a58
Jan <janpgregor@gmail.com>
2022-03-12 18:10:53 +0100

    fixed doc string

--- src/functions/task_generator.py
+++ src/functions/task_generator.py
@@ -4,11 +4,11 @@ import random
 
 
 def generate_task() -> TransportTask:
-    """ For the daily transport task generates lists of random:
+    """ For the daily transport task generates lists of:
             3 to 6 trucks of red, green or blue colour, parked in randomly chosen warehouse,
             400 to 600 points of coordinates x & y in range [0, 100]
                 and cargo amount of [-200 to -100]U[100 to 200] kg of random cargo type for each,
-            5 warehouses chosen from the points list.
+            5 warehouses randomly chosen from the points list.
 
     :return TransportTask: generated transport task to solve later
     """
