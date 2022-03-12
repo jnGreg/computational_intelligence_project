f979614d6e2cf38b1c3b56021c2e53843643b16f
Jan <janpgregor@gmail.com>
2022-03-12 18:44:19 +0100

    removed starting trucks list

--- src/classes/warehouse.py
+++ src/classes/warehouse.py
@@ -3,14 +3,5 @@ from src.classes.point import Point
 
 
 class Warehouse(Point):
-    def __init__(self, x: int, y: int, cargo_amount: int, cargo_type: str, starting_trucks: list):
+    def __init__(self, x: int, y: int, cargo_amount: int, cargo_type: str):
         super().__init__(x, y, cargo_amount, cargo_type)
-        self.starting_trucks = starting_trucks
-
-    @property
-    def starting_trucks(self) -> list:
-        return self._starting_trucks
-
-    @starting_trucks.setter
-    def starting_trucks(self, value: list) -> None:
-        self._starting_trucks = value
