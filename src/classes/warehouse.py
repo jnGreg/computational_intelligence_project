4f5dedc550266b2877aab53b2f35b77574adffba
Jan <janpgregor@gmail.com>
2022-03-13 15:42:06 +0100

    removed cargo from warehouse class

--- src/classes/warehouse.py
+++ src/classes/warehouse.py
@@ -1,7 +1,21 @@
 
-from src.classes.point import Point
+class Warehouse:
+    def __init__(self, x: int, y: int):
+        self._x = x
+        self._y = y
 
+    @property
+    def x(self) -> int:
+        return self._x
 
-class Warehouse(Point):
-    def __init__(self, x: int, y: int, cargo_amount: int, cargo_type: str):
-        super().__init__(x, y, cargo_amount, cargo_type)
+    @x.setter
+    def x(self, value: int) -> None:
+        self._x = value
+
+    @property
+    def y(self) -> int:
+        return self._y
+
+    @y.setter
+    def y(self, value: int) -> None:
+        self._y = value
