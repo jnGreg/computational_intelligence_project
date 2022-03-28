7883a3d6220470077f9ceeb59a80b13474fa5b6c
BartlomiejPlata <bartlomiej,plata@edu.uekat.pl>
2022-03-28 20:04:54 +0200

    add distance matrix function

---
+++ src/functions/gen_dist_matrix.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from scipy.spatial import distance
+from task_generator import generate_task
+import math
+
+def calc_euc_dist(p1, p2):
+    """
+    function to calculate the distance between two coordinate points
+    p1 - point object
+    p2 - point object
+     :return distance: distance beetwen two points od coordintes
+     """
+    point_1 = p1[0:2]
+    point_2 = p2[0:2]
+    distance = math.sqrt((point_1[0] - point_2[0])**2 + (point_1[1] - point_2[1])**2)
+    return distance
+
+
+def generate_dist_matrix(list_of_points):
+    """
+    function for generating distance matrices from coordinates of points
+    list_of_points: list of objects (Point)
+         :return df_mat_dis: dataframe with distance matrix
+         """
+    cords = []
+    n = 0
+    for p in list_of_points:
+        cords.append(p[0:2])
+        n+=1
+    mat_dist = distance.cdist(cords, cords, 'euclidean')
+    df_mat_dist = pd.DataFrame(mat_dist)
+    return df_mat_dist
+
+#test
+#T = generate_task()
+#B = generate_dist_matrix(T.points)
+#print(B)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-
-from src.functions.task_generator import generate_task
+from functions.gen_dist_matrix import generate_dist_matrix
+from functions.task_generator import generate_task
 
 
 def main():
-    generate_task()
+    T = generate_task()
+    generate_dist_matrix(T.points)
 
 
 if __name__ == "__main__":
