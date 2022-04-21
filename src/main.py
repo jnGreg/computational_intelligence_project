bd69debfd7807a1ba8c493c4a3099cca2e4fb18f
MaciekG <maciek.1998.g@gmail.com>
2022-04-21 19:35:50 +0200

    main showing load, unload

--- src/main.py
+++ src/main.py
@@ -1,10 +1,30 @@
-from functions.gen_dist_matrix import generate_dist_matrix
-from functions.task_generator import generate_task
 
+# from functions.gen_dist_matrix import generate_dist_matrix
+from functions.task_generator import generate_task
+from functions.load_truck import load_truck
+from functions.unload_truck import unload_truck
 
 def main():
-    T = generate_task()
-    generate_dist_matrix(T.points)
+    points=generate_task().points
+    cars=generate_task().trucks
+
+    point=points[0]
+    car=cars[0]
+
+    print('init')
+    print(point)
+    print(car)
+
+    print('ŁADUJEMY')
+    car,point = load_truck(car,point)
+    print(point)
+    print(car)
+
+    print('UNŁADUJEMY')
+    car,point = unload_truck(car,point)
+
+    print(point)
+    print(car)
 
 
 if __name__ == "__main__":
