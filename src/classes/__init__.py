a75c290ec5a219daef6c51b5b0ca3e3cdbf18360
MaciekG <maciek.1998.g@gmail.com>
2022-04-21 19:32:56 +0200

    classes adj

---
+++ src/classes/__init__.py
@@ -0,0 +1,3 @@
+from classes.point import Point
+from classes.transport_task import TransportTask
+from classes.truck import Truck
--- src/classes/point.py
+++ src/classes/point.py
@@ -1,26 +1,35 @@
 
-class Point:
-    def __init__(self, x: int, y: int, cargo_amount: int, cargo_type: str):
-        self._x = x
-        self._y = y
-        self._cargo_amount = cargo_amount
-        self._cargo_type = cargo_type
+class Point():
+    def __init__(self,location: tuple, magazine: bool, cargo_amount: int) -> None:
+        self.location=location
+        self.magazine=magazine
+        self.status='active' if self.magazine==False else 'magazine'
+        self.cargo_amount=cargo_amount if self.magazine==False else 0
+            
 
     @property
-    def x(self) -> int:
-        return self._x
+    def location(self) -> int:
+        return self._location
 
-    @x.setter
-    def x(self, value: int) -> None:
-        self._x = value
+    @location.setter
+    def location(self, value: int) -> None:
+        self._location = value
 
     @property
-    def y(self) -> int:
-        return self._y
+    def magazine(self) -> int:
+        return self._magazine
 
-    @y.setter
-    def y(self, value: int) -> None:
-        self._y = value
+    @magazine.setter
+    def magazine(self, value: int) -> None:
+        self._magazine = value
+
+    @property
+    def status(self) -> int:
+        return self._status
+
+    @status.setter
+    def status(self, value: int) -> None:
+        self._status = value
 
     @property
     def cargo_amount(self) -> int:
@@ -30,6 +39,7 @@ class Point:
     def cargo_amount(self, value: int) -> None:
         self._cargo_amount = value
 
+
     @property
     def cargo_type(self) -> str:
         return self._cargo_type
@@ -38,3 +48,11 @@ class Point:
     def cargo_type(self, value: str) -> None:
         self._cargo_type = value
 
+    def __str__(self) -> str:
+        return f""" 
+        {'Magazine' if self.magazine==True else 'Point'}
+            Location: {self.location}
+            Order: {self.cargo_amount} kg
+
+
+        """
