8c656dd196e3d7f7d3a242e43e87633300d15920
Jan <janpgregor@gmail.com>
2022-04-22 15:11:04 +0200

    merged to sprint 3.1

--- src/main.py
+++ src/main.py
@@ -1,13 +1,35 @@
 from functions.gen_dist_matrix import generate_dist_matrix
 from functions.task_generator import generate_task
+from functions.load_truck import load_truck
+from functions.unload_truck import unload_truck
 
 
 def main():
     T = generate_task()
     generate_dist_matrix(T.points)
+    # print(generate_dist_matrix(T.points))
 
+    points = T.points
+    cars = T.trucks
 
-    print(generate_dist_matrix(T.points))
+    point = points[0]
+    car = cars[0]
+
+    print('init')
+    print(point)
+    print(car)
+
+    print('Load truck')
+    car, point = load_truck(car, point)
+    print(point)
+    print(car)
+
+    print('Unload truck')
+    car, point = unload_truck(car, point)
+
+    print(point)
+    print(car)
+
 
 if __name__ == "__main__":
     main()
