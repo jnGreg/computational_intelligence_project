2dfc63717447a1ee35d0fbe6a4041ee096ec0695
MaciekG <maciek.1998.g@gmail.com>
2022-04-23 10:06:24 +0200

    generate task, simple load, unload truck, vrp

--- src/main.py
+++ src/main.py
@@ -2,13 +2,12 @@ from functions.gen_dist_matrix import generate_dist_matrix
 from functions.task_generator import generate_task
 from functions.load_truck import load_truck
 from functions.unload_truck import unload_truck
-
-
+from functions.vrp_solution import vrp
 def main():
-    T = generate_task()
-    generate_dist_matrix(T.points)
-    # print(generate_dist_matrix(T.points))
-
+    T = generate_task(100,500)
+    print(generate_dist_matrix(T.points))
+    # # print(generate_dist_matrix(T.points))
+    ## Car logic test
     points = T.points
     cars = T.trucks
 
@@ -29,6 +28,10 @@ def main():
 
     print(point)
     print(car)
+
+    #### VRP solution
+    print('VRP')
+    vrp()
 
 
 if __name__ == "__main__":
